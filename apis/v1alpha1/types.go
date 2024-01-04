2a9b239026deb5cb913a7ea3e720cfbdf791e7e0
smcavallo <smcavallo@hotmail.com>
2024-01-04 16:36:02 -0500

    add support for region Signed-off-by: smcavallo <smcavallo@hotmail.com>

--- apis/v1alpha1/types.go
+++ apis/v1alpha1/types.go
@@ -27,6 +27,9 @@ type ProviderConfigSpec struct {
 	Credentials ProviderCredentials `json:"credentials"`
 	// AccountID required by nerdgraph API
 	AccountID string `json:"account_id,omitempty"`
+	// Region of the account
+	// +kubebuilder:validation:Enum=US;EU
+	Region *string `json:"region,omitempty"`
 }
 
 // ProviderCredentials required to authenticate.
